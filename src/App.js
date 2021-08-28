06d3c32757fba9cbc14f398ef8c264917f5ba365
hermitcrab <hermitcrab@pop-os.localdomain>
2021-08-29 01:48:04 +0700

    mockup GUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,199 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Todo</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/684/600/400" alt="photo" />
+              Quis culpa reprehenderit et aliqua velit sit qui tempor do elit.
+            </li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+            <li>Pick a task to take very first step</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+      </div>
     </div>
   );
 }
